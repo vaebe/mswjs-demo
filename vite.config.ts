2175e1b928fc5960ecc23d7bbc50ea03a4793352
vaebe <18137693952@163.com>
2024-12-04 22:23:27 +0800

    chore: 静态页面部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,33 @@ import importPlugin from '@opentiny/vue-vite-import'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    autoImportPlugin('vite'),
-    importPlugin(
-      [
-        {
-          libraryName: '@opentiny/vue',
-        },
-        {
-          libraryName: `@opentiny/vue-icon`,
-          customName: (name) => {
-            return `@opentiny/vue-icon/lib/${name.replace(/^icon-/, '')}.js`
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'development' ? '/' : '/mswjs-demo/',
+    plugins: [
+      vue(),
+      autoImportPlugin('vite'),
+      importPlugin(
+        [
+          {
+            libraryName: '@opentiny/vue',
           },
-        },
-      ],
-      'pc',
-    ),
-  ],
-  resolve: {
-    alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
-  },
-  define: {
-    // eslint-disable-next-line node/prefer-global/process
-    'process.env': { ...process.env, TINY_MODE: 'pc' },
-  },
+          {
+            libraryName: `@opentiny/vue-icon`,
+            customName: (name) => {
+              return `@opentiny/vue-icon/lib/${name.replace(/^icon-/, '')}.js`
+            },
+          },
+        ],
+        'pc',
+      ),
+    ],
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    },
+    define: {
+      // eslint-disable-next-line node/prefer-global/process
+      'process.env': { ...process.env, TINY_MODE: 'pc' },
+    },
+  }
 })
