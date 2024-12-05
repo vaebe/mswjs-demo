f7747a08487a7027fdadd7fc45d27b26557cf5ad
vae <18137693952@163.com>
2024-12-05 13:10:33 +0800

    fix: serviceWorker 文件路径

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import { defineAsyncComponent } from 'vue'
 
+const { VITE_APP_TITLE } = import.meta.env
+document.title = VITE_APP_TITLE
+
 const Crud = defineAsyncComponent(() => import('@/components/Crud.vue'))
 </script>
 
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,14 +1,14 @@
 import { setupWorker } from 'msw/browser'
 import { allHandlers } from './handlers'
-// import { getBasePath } from './utils'
+import { getBasePath } from './utils'
 
 export const worker = setupWorker(...allHandlers)
 
 export function initMswWorker() {
   worker.start({
     onUnhandledRequest: 'bypass',
-    // serviceWorker: {
-    //   url: `${getBasePath()}/mockServiceWorker.js`,
-    // },
+    serviceWorker: {
+      url: `${getBasePath()}/mockServiceWorker.js`,
+    },
   })
 }
